cd6898cabaf083aa732826bd9bb1765cc8bf2374
Goncalo Pereira <goncalosilva2008@protonmail.com>
2026-06-24 20:41:39 +0100

    Added feature to register a new Customer.

---
+++ Helpers/CustomerHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeskFlow.Models;
+
+namespace DeskFlow.Helpers
+{
+    public class CustomerHelper
+    {
+        public void Insert(Customer customer)
+        {
+            App.lstUsers.Add(customer);
+        }
+    }
+}
--- Views/RegisterWindow.xaml.cs
+++ Views/RegisterWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DeskFlow.Models;
+using DeskFlow.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,12 +28,48 @@ namespace DeskFlow.Views
 
         private void btnBackToLogin_Click(object sender, RoutedEventArgs e)
         {
+            LoginWindow loginWin = new LoginWindow();
+            loginWin.Show();
 
+            this.Close();
         }
 
         private void btnRegister_Click(object sender, RoutedEventArgs e)
         {
+            CustomerHelper cH = new CustomerHelper();
 
+            string _name = txtFullName.Text.Trim();
+            string _email = txtEmail.Text.Trim();
+            string _password = txtPassword.Password.Trim();
+            string _confirmPass = txtConfirmPassword.Password.Trim();
+
+            if (_password != _confirmPass)
+            {
+                MessageBox.Show("As passwords introduzidas não coincidem!", "Erro no Registo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                Customer newClient = new Customer();
+
+                newClient.Name = _name;
+                newClient.Email = _email;
+                newClient.Password = _password;
+
+                cH.Insert(newClient);
+
+                MessageBox.Show("Conta criada com sucesso! Já pode fazer login.", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                LoginWindow loginWin = new LoginWindow();
+                loginWin.Show();
+
+                this.Close();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro de Validação", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
